5842e263962e41d99ae2000ff59e41da9423bd87
Gabriele Masina <gabriele.masina@virgilio.it>
2025-11-26 09:02:05 +0100

    Add more test instances, one is failing

--- tests/test_lemmas_correctness.py
+++ tests/test_lemmas_correctness.py
@@ -3,8 +3,8 @@ from typing import NamedTuple
 import pytest
 from pysmt.fnode import FNode
 from pysmt.formula import FormulaContextualizer
-from pysmt.shortcuts import And, LE, LT, Or, Solver, Symbol, read_smtlib
-from pysmt.typing import REAL
+from pysmt.shortcuts import And, Iff, Not, Or, Solver, Symbol, Xor, read_smtlib
+from pysmt.typing import BOOL, REAL
 
 from theorydd.formula import get_normalized
 from theorydd.solvers.mathsat_partial_extended import MathSATExtendedPartialEnumerator
@@ -14,17 +14,60 @@ from theorydd.walkers.walker_refinement import RefinementWalker
 
 w, x, y, z = (Symbol(name, REAL) for name in "wxyz")
 
-Example = NamedTuple(
-    "Example", [("formula", str | FNode), ("is_sat", bool), ("tlemmas_expected", bool)]
-)
+xp3yp2zp1LExp3yp2z = x + 3 * y + 2 * z + 1 <= x + 3 * y + 2 * z
+xp3yp2zp1LE05 = x + 3 * y + 2 * z + 1 <= 0.5
+xp3yp2zLE05 = x + 3 * y + 2 * z <= 0.5
+xp3yLE05 = x + 3 * y <= 0.5
+yLE05 = y <= 0.5
+xLE05 = x <= 0.5
+a, b = (Symbol(name, BOOL) for name in "ab")
+
+Example = NamedTuple("Example", [("formula", str | FNode), ("is_sat", bool), ("tlemmas_expected", bool)])
+
 
 # formulas to test: (formula, is_sat, tlemmas_expected)
 EXAMPLES: list[Example] = [
+    Example(xLE05, True, False),
+    Example(And(xLE05, yLE05), True, False),
+    Example(Or(xLE05, yLE05), True, False),
+    Example(And(xLE05, yLE05, xp3yLE05, xp3yp2zLE05), True, False),
+    Example(Or(xLE05, yLE05, xp3yLE05, xp3yp2zLE05), True, True),
+    Example(Not(xLE05), True, False),
+    Example(Not(Not(xLE05)), True, False),
+    Example(Not(Not(And(xLE05, yLE05))), True, False),
+    Example(Not(Not(Or(xLE05, yLE05))), True, False),
+    Example(And(x <= y, y <= x), True, False),
+    Example(And(x < y, y < x), False, True),
+    Example(And(x <= y, z <= w), True, False),
+    Example(Or(x < y, y < z, z < x), True, True),
+    Example(And(Or(xLE05, yLE05), Or(xp3yLE05, xp3yp2zLE05)), True, False),
+    Example(Or(And(xLE05, yLE05), And(xp3yLE05, xp3yp2zLE05)), True, True),
+    Example(Not(Or(xLE05, Not(And(yLE05, Not(Or(xp3yLE05, xp3yp2zLE05)))))), True, False),
+    Example(
+        Or(
+            Or(Or(And(xLE05, yLE05), And(xp3yLE05, xp3yp2zLE05)), And(xp3yp2zp1LE05, a)),
+            Not(And(b, xp3yp2zp1LExp3yp2z)),
+        ),
+        True,
+        True,
+    ),
+    Example(Or(And(xLE05, yLE05), And(xp3yLE05, Or(Not(And(xLE05, yLE05)), xp3yp2zLE05))), True, True),
+    Example(Iff(And(xp3yLE05, xp3yp2zLE05), Or(xp3yp2zLE05, And(yLE05, xLE05))), True, True),
+    Example(
+        Or(And(xp3yLE05, xp3yp2zLE05), Not(Iff(And(xp3yLE05, xp3yp2zLE05), Or(xp3yp2zLE05, And(yLE05, xLE05))))),
+        True,
+        True,
+    ),
+    Example(
+        Xor(
+            And(Not(And(Not(xLE05), xp3yLE05)), Or(Not(yLE05), Not(xp3yp2zLE05))),
+            And(Not(And(xp3yp2zLE05, Not(a))), Not(And(Not(yLE05), a))),
+        ),
+        True,
+        True,
+    ),
     Example("tests/items/test_lemmas.smt2", True, True),
-    Example(And(LE(x, y), LE(y, x)), True, False),
-    Example(And(LT(x, y), LT(y, x)), False, True),
-    Example(And(LE(x, y), LE(z, w)), True, False),
-    Example(Or(LT(x, y), LT(y, z), LT(z, x)), True, True),
+    Example("tests/items/6_1.smt2", True, True),
 ]
 
 SOLVERS = [
@@ -72,6 +115,14 @@ def test_lemmas_correctness(example, solver):
     bool_walker = BooleanAbstractionWalker(atoms=phi_and_lemmas_atoms)
     phi_and_lemmas_abstr = bool_walker.walk(phi_and_lemmas)
     phi_abstr = bool_walker.walk(phi)
+
+    # ---- Check that phi and phi & lemmas are theory-equivalent ----
+    with Solver() as check_solver:
+        check_solver.push()
+        check_solver.add_assertion(Xor(phi, phi_and_lemmas))
+        sat = check_solver.solve()
+        assert not sat, "Phi and Phi & lemmas should be theory-equivalent"
+        check_solver.pop()
 
     # ---- Check that every truth assignment of phi & lemmas is theory-sat ----
     solver_abstr = MathSATTotalEnumerator()
